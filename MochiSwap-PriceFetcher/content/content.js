1218ac2d5f07e025b6cab59165105eb5211cc231
strajk- <7372902+strajk-@users.noreply.github.com>
2021-04-23 21:28:11 +0200

    Update content.js

--- MochiSwap-PriceFetcher/content/content.js
+++ MochiSwap-PriceFetcher/content/content.js
@@ -1,15 +1,9 @@
 setInterval(function(){ 
-	var inputContainer = document.getElementsByClassName("sc-bFADNz hzhxvm token-amount-input")[0];
+	var inputContainer = document.getElementsByClassName("token-amount-input")[1];
 	if(inputContainer.value > 0){
-		var addrContainer = document.getElementsByClassName("css-1qqnh8x")[5];
-		var val = addrContainer.innerText.split(' ')[0];
-		if(val > 0){
-			var currentdate = new Date(); 
-			var datetime = currentdate.getDate() + "/"+ (currentdate.getMonth()+1)  + "/" + currentdate.getFullYear() + " "  + currentdate.getHours() + ":"  + currentdate.getMinutes() + ":" + currentdate.getSeconds();
-			var output = datetime + ";" + val;
-			console.log(output);
-		}
+		var currentdate = new Date(); 
+		var datetime = currentdate.getDate() + "/"+ (currentdate.getMonth()+1)  + "/" + currentdate.getFullYear() + " "  + currentdate.getHours() + ":"  + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+		var output = datetime + ";" + inputContainer.value;
+		console.log(output);
 	}
 }, 5000);
-
-
